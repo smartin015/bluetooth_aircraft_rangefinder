0f9ab0e12ea8e0db37403bf8495f9fc763d5a80d
Scott Martin <smartin015@gmail.com>
2019-08-04 09:24:29 -0400

    Cleanup BLE rangefinder arduino code

--- firmware/ble_rangefinder/ble_rangefinder.ino
+++ firmware/ble_rangefinder/ble_rangefinder.ino
@@ -1,3 +1,22 @@
+/* Lolin D32 Bluetooth LE Rangefinder
+ *  Publishes the sensor data from a TFMini Plus IR distance sensor.
+ *  See https://cdn.sparkfun.com/assets/1/4/2/1/9/TFmini_Plus_A02_Product_Manual_EN.pdf
+ *  for the product manual.
+ *  
+ *  It uses a Lolin D32 board which runs an ESP32-WROOM-32 packaged wifi+BLE microcontroller.
+ *  See https://wiki.wemos.cc/products:d32:d32 for controller details.
+ *  
+ *  This was built using the following libraries:
+ *  - TFMPlus 1.3.4
+ *  - ESP32 BLE Arduino 1.0.1
+ *  
+ *  The TFMPlus has four wires. Wiring is as follows:
+ *  - RED   (TFMPlus VCC) -- USB (Lolin D32)
+ *  - BLACK (TFMPlus GND) -- GND (Lolin D32)
+ *  - WHITE (TFMPlus RXD) <- 15  (Lolin D32 TXD)
+ *  - GREEN (TFMPlus TXD) -> 4   (Lolin D32 RXD)
+ */
+
 #include <TFMPlus.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -5,28 +24,30 @@
 #include <BLE2902.h>
 
 TFMPlus tfmP;
-BLEServer *pServer = NULL;
-BLECharacteristic * pDistCharacteristic;
-BLE2902 * pDistCCCD;
-BLECharacteristic * pFluxCharacteristic;
-BLECharacteristic * pTempCharacteristic;
-BLECharacteristic * pStatusCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
+#define D32_RXD 4
+#define D32_TXD 15
+#define SAMPLE_RATE_HZ 10
+#define TEMP_FLUX_REPORT_PD 20
 uint16_t tfDist;       // Distance measurement in centimeters (default)
 uint16_t tfFlux;       // Luminous flux or intensity of return signal
 uint16_t tfTemp;       // Temperature in degrees Centigrade (coded)
 uint16_t loopCount;    // Loop counter (1-20)
 uint64_t lastSample = 0;
 
+BLEServer *pServer = NULL;
+BLE2902 * pDistCCCD;
+BLECharacteristic * pDistCharacteristic;
+BLECharacteristic * pFluxCharacteristic;
+BLECharacteristic * pTempCharacteristic;
+BLECharacteristic * pStatusCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_DIST "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_FLUX "6E400004-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TEMP "6E400005-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_STATUS "6E400006-B5A3-F393-E0A9-E50E24DCCA9E"
-
-#define SAMPLE_RATE_HZ 10
-#define TEMP_FLUX_REPORT_PD 20
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -83,11 +104,9 @@ void setupBLE() {
 }
 
 void setupTFMP() {
-  Serial1.begin(115200, SERIAL_8N1, 4, 15); //Baud rate, parity mode, RX, TX
+  Serial1.begin(115200, SERIAL_8N1, D32_RXD, D32_TXD); //Baud rate, parity mode, RX, TX
   delay(20); // Give port time to initalize
   tfmP.begin( &Serial1);
-
-  printTFMPFirmwareVersion();
 }
 
 void setup() {
@@ -103,6 +122,7 @@ void setup() {
   
   Serial.println("Setting up TFMPlus");
   setupTFMP();
+  printTFMPFirmwareVersion();
 
   pServer->getAdvertising()->start();
   Serial.println("Waiting for client...");
@@ -117,7 +137,6 @@ void onDisconnect() {
 void onConnect() {
   oldDeviceConnected = deviceConnected;
 }
-
 
 void loopTFMP() {
   if (!tfmP.getData(tfDist, tfFlux, tfTemp)) {
@@ -203,7 +222,7 @@ void loop() {
     return;
   }
   
-  if (deviceConnected && !oldDeviceConnected) { // Connect
+  if (deviceConnected && !oldDeviceConnected) {
     onConnect();
     return;
   }
